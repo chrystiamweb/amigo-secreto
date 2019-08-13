15d093fc4050ed040dc435159b3055ee2d3793fb
Chrystiam Andrade <chrystiam.andrade@gmail.com>
2019-08-13 09:34:02 -0300

    project setup

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,6 +2,7 @@
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
+
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
@@ -58,4 +59,5 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 end
