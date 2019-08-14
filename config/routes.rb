c20d38c1605aab18a15be7be71f2299858643c2c
Chrystiam Andrade <chrystiam.andrade@gmail.com>
2019-08-14 11:48:45 -0300

    add sidekick routes and config

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  mount Sidekiq::Web => 'sidekiq'  
 end
