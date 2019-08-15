5106d4be88439c6b237d0533a9942b388fc4b09d
Chrystiam Andrade <chrystiam.andrade@gmail.com>
2019-08-15 13:34:09 -0300

    generated controller Campaigns

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
+  get 'campaigns/show'
+
+  get 'campaigns/index'
+
+  get 'campaigns/create'
+
+  get 'campaigns/update'
+
+  get 'campaigns/destroy'
+
+  get 'campaigns/raffle'
+
   get 'pages/home'
 
   devise_for :users, :controllers => { registrations: 'registrations' }
